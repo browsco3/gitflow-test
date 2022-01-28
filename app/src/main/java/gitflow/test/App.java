7904fe1b6af2baf6d0ee414c2d8ecd4592b1676b
Scott Brown <browsco3@isu.edu>
2022-01-28 14:44:22 -0700

    Added the first-feature branch in dev. \nChanged the App.java file

--- app/src/main/java/gitflow/test/App.java
+++ app/src/main/java/gitflow/test/App.java
@@ -10,5 +10,6 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
+	System.out.println("This git thing is starting to get complicated, but I'm getting the hang of it.");
     }
 }
